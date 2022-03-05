6e611b8316105de4009c3d3330e92eac781002c8
lissalad <elissalaymon@gmail.com>
2022-03-04 22:59:31 -0800

    tweets

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 """Main script, uses other modules to generate sentences."""
-from flask import Flask, render_template
+from flask import Flask, request, redirect, render_template
 from dictogram import Dictogram
 from markov import Markov
 from tokens import tokenize
+import twitter
 
 app = Flask(__name__)
 
@@ -11,17 +12,20 @@ histogram = Dictogram(words)
 
 markov = Markov(histogram, words)
 
-
-
 @app.before_first_request
 def before_first_request():
     """Runs only once at Flask startup"""
   
 
-
 @app.route("/")
 def home():
     return render_template("index.html", tweet=markov.sentence(histogram, 20))
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    status = request.form['sentence']
+    twitter.tweet(status)
+    return redirect('/')
 
 
 if __name__ == "__main__":
